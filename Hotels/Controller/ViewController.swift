da7a8e790540fad2419c22e1d958ead3be1ac1f2
Eugene <Eugeniolio@yandex.com>
2021-06-04 00:14:19 +0300

    realized UI API Integration

--- Hotels/Controller/ViewController.swift
+++ Hotels/Controller/ViewController.swift
@@ -7,24 +7,40 @@
 
 import UIKit
 
-class ViewController: UIViewController,UITableViewDelegate, UITableViewDataSource {
+class ViewController: UITableViewController{
    
     let hotels = HotelsAPI()
     
     private var dataHotels =  [HotelsStruct]()
-
+    
 
     override func viewDidLoad() {
         
         super.viewDidLoad()
-    
+        
+        tableView.register(UITableViewCell.self,forCellReuseIdentifier: "cell")
+        
+        tableView.dataSource = self
+        
+        tableView.delegate = self
         // Do any additional setup after loading the view.
         hotels.delegate = self
         
         hotels.callAPI()
         
-             
     }
+    
+// MARK: - TableViewDataSource
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = dataHotels[indexPath.row].name
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dataHotels.count
+    }
+    
 }
 
 
@@ -34,8 +50,10 @@ extension ViewController: HotelsManagerDelegate{
  
     // Update hotels delegate
     func didUpdateHotels(hotels: [HotelsStruct]) {
-        self.dataHotels = hotels
-        
+            self.dataHotels = hotels
+        DispatchQueue.main.async {
+            self.tableView.reloadData()
+        }
     }
     
     // Error handler
